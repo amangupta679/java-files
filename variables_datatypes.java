24b6b55ce4f6c03ec63fc184b392f13298fdd2a6
Aman Gupta <56467452+amangupta679@users.noreply.github.com>
2023-05-28 21:52:15 +0530

    Create variables_datatypes.java (#1)

---
+++ variables_datatypes.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public  class variables_datatypes {
+    public static void main(String[] args){
+        // write a java program to add to number
+        System.out.println("enter the two nnumbers to add");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the ffirst number");
+        int num1 = sc.nextInt();
+        System.out.println("eenter the second number");
+        int num2 = sc.nextInt();
+//        int num1 = 6 ;
+//        int num2  = 10;
+        System.out.println(num1 + num2);
+    }
+
+}
